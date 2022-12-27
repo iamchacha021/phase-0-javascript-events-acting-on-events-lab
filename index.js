8f5f34d51c355185c553f1c9ae2c71ff0d09bc2b
iamchacha021 <iamchacha021@gmail.com>
2022-12-27 17:25:49 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor= 'red'
+dodger.style.bottom ='0px'
+// dodger.style.left ='0px'
+
+document.addEventListener('keydown', (ev)=>{
+    console.log(ev)
+    if(ev.key ==='ArrowLeft'){
+        moveDodgerLeft();
+    }
+    else if (ev.key ==='ArrowRight'){
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+
+    if(left<360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
